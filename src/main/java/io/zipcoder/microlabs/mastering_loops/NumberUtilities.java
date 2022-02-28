2c5129f94d0e3c316f16a30940d33b6468e8a62a
Miles Coney <MilesEConey@gmail.com>
2022-02-27 22:47:19 -0500

    completed number utilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -2,35 +2,72 @@ package io.zipcoder.microlabs.mastering_loops;
 
 
 public class NumberUtilities {
+    static double output;
+
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+       String output = "";
+        for (int i = start; i < stop; i++) {
+             if (i % 2 == 0) {
+                output += i; //This is the same as output = output + i
+             }
+        }
+        return output;
     }
 
 
+
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String output = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 1) {
+                output += i; //This is the same as output = output + i
+            }
+        }
+        return output;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String output = "";
+        for (int i = start; i < stop; i += step) {
+           output += (int) Math.pow(i,2);
+        }
+        return output;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getExponentiations(int start, int stop, int step, int exponent) {
+        String output = "";
+        for (int i = start; i < stop; i += step) {
+            output += (int) Math.pow(i,exponent);
+        }
+        return output;
+    }
+
+    public static String getRange(int stop) {
+        String output = "";
+        for (int i = 0; i < stop; i++) {
+            output += i;
+        }
+        return output;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String output = "";
+        for (int i = start; i < stop; i++) {
+            output += i;
+        }
+        return output;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String output = "";
+        for (int i = start; i < stop; i += step) {
+            output += i;
+        }
+        return output;
     }
 
 
-    public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
-    }
+
 }
