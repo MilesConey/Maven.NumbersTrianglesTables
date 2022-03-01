a165f5e2434a5d0d92bde558fb3e56c12d151120
Miles Coney <MilesEConey@gmail.com>
2022-02-28 22:53:53 -0500

    Updated TriangleUtilities and passed all tests. Finished with lab

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,29 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String newTriangle = "";
+        for(int i = 0; i < numberOfRows ; i++){
+            for (int j = 0; j <= i; j++){
+                newTriangle += "*";
+            }
+            newTriangle += "\n";
+        }
+        return newTriangle;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String newRow = "";
+        for(int i = 0; i < numberOfStars; i++){
+            newRow += "*";
+        }
+        return newRow;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
